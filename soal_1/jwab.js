fb09d999482cbdb1ae1a037ce8ee913c494e72e4
erwinbaee <erwinsajaya131002@gmail.com>
2021-08-07 10:26:11 +0700

    Evaluasi_sprint3

---
+++ soal_1/jwab.js
@@ -0,0 +1,8 @@
+let santriIkhwan = [`shidiq`, `rahmad`, , , `Shofyan`, `aldi`, `Kardi`];
+
+const [Lampung, Prabumulih, Medan, Ceribon, ...Lain] = santriIkhwan;
+
+console.log(Lampung);
+console.log(Prabumulih);
+console.log(Lain);
+
---
+++ soal_4/jwab.js
@@ -0,0 +1,29 @@
+class Animal  {
+    constructor() {
+        this.name = name 
+        this.isMammal = this.isMammal
+
+    }
+
+}
+
+class Ape {
+    constructor() {
+        function tampil() {
+            yell()
+
+            return Auooo
+            
+        }
+    }
+}
+
+class Flog {
+    constructor() {
+        function  musik() {
+            jump ()
+            return hiphop
+            
+        }
+    }
+}
